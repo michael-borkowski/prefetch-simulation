01da2be8eb68cef76f4ac05f552c80022115240e
Michael Borkowski <michael@borkowski.at>
2015-03-16 18:08:32 +0100

    chore(ci): ci components

---
+++ ci/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+RETRIES=1
+
+if [[ "$1" == "--travis" ]]; then
+	RETRIES=3
+fi
+
+function build() {
+	cd "$DIR/../spiceJ"
+	# -P ci to activate release-like things like javadoc generation (and warnings)
+	mvn -P ci clean verify
+}
+
+
+
+try=0
+
+while [ $try -lt $RETRIES ]; do
+	build && break
+	((try++))
+done
+
+[ $try -eq 3 ] && exit 1
+exit 0
