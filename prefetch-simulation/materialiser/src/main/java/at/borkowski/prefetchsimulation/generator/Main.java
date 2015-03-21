cdb39154344e515a19f8d97e84edafbfdc5e9ef7
Michael Borkowski <michael@borkowski.at>
2015-03-21 22:40:52 +0100

    feat(materialiser): materialise + paint operations

--- prefetch-simulation/materialiser/src/main/java/at/borkowski/prefetchsimulation/generator/Main.java
+++ prefetch-simulation/materialiser/src/main/java/at/borkowski/prefetchsimulation/generator/Main.java
@@ -1,17 +1,14 @@
 package at.borkowski.prefetchsimulation.generator;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.io.IOException;
+import java.io.InputStream;
 
-import at.borkowski.prefetchsimulation.Request;
-import at.borkowski.prefetchsimulation.algorithms.PrefetchAlgorithm;
-import at.borkowski.prefetchsimulation.algorithms.RespectRatePredictionAlgorithm;
 import at.borkowski.prefetchsimulation.configuration.Configuration;
-import at.borkowski.prefetchsimulation.configuration.RequestSeries;
-import at.borkowski.prefetchsimulation.configuration.distributions.Distributions;
+import at.borkowski.prefetchsimulation.configuration.ConfigurationException;
+import at.borkowski.prefetchsimulation.configuration.ConfigurationReader;
 import at.borkowski.prefetchsimulation.genesis.Genesis;
+import at.borkowski.prefetchsimulation.genesis.GenesisException;
+import at.borkowski.prefetchsimulation.genesis.GenesisReader;
 import at.borkowski.prefetchsimulation.genesis.GenesisWriter;
 import at.borkowski.prefetchsimulation.painter.GenesisPainter;
 import at.borkowski.prefetchsimulation.painter.PaintResult;
@@ -19,37 +16,42 @@ import at.borkowski.prefetchsimulation.painter.Saver;
 
 public class Main {
    public static void main(String[] args) throws Exception {
-      long totalTicks = 1000 * 3600 * 2;
-      int maximumByterate = 1000;
-      long networkQualityPhaseLength = 1000 * 60 * 3;
-      double networkUptime = 0.95;
-      double networkByterateStability = 0.95;
-      int networkByterateVariability = 20;
-      double predictionAccuracy = 0.8;
-      Collection<RequestSeries> recurringSeries = new LinkedList<>();
-      Collection<Request> intermittentRequests = new LinkedList<>();
-      long lookAheadTime = totalTicks;
-      Class<? extends PrefetchAlgorithm> algorithm = RespectRatePredictionAlgorithm.class;
+      if (args.length != 1) {
+         System.err.println("Must supply exactly one argument (operation), not " + args.length);
+         usage();
+         return;
+      }
+      String operation = args[0];
 
-      intermittentRequests.add(new Request(1000 * 60 * 30, 1, 500));
-      recurringSeries.add(new RequestSeries(Distributions.uniformLong(1000 * 60 * 10, 1000 * 60 * 15), Distributions.uniformInteger(1 * 1000, 3 * 1000), Distributions.uniformInteger(500, 1000), Distributions.exactly(0L), Distributions.exactly(totalTicks)));
+      if (operation.equals("generate-genesis")) {
+         Genesis genesis = createGenesis(System.in);
+         GenesisWriter genesisWriter = new GenesisWriter(System.out);
+         genesisWriter.write(genesis);
+      } else if (operation.equals("draw-png")) {
+         Genesis genesis = readGenesis(System.in);
+         PaintResult result = GenesisPainter.paint(genesis);
+         Saver.savePNG(result, System.out);
+      } else {
+         System.err.println("Unknown operation: " + operation);
+         usage();
+      }
+   }
 
-      Configuration configuration = new Configuration(totalTicks, maximumByterate, networkQualityPhaseLength, networkUptime, networkByterateStability, networkByterateVariability, predictionAccuracy, recurringSeries, intermittentRequests, algorithm, lookAheadTime);
+   private static Genesis createGenesis(InputStream in) throws IOException, ConfigurationException {
+      ConfigurationReader configurationReader = new ConfigurationReader(in);
+      Configuration configuration = configurationReader.read();
 
       GenesisGenerator generator = new GenesisGenerator(configuration);
-      if (!configuration.hasSeed())
-         generator.seed(314159);
-      Genesis genesis = generator.generate();
+      return generator.generate();
+   }
 
-      String outputFile = "/home/michael/tmp-genesis";
-      try (OutputStream os = new FileOutputStream(outputFile)) {
-         new GenesisWriter(os).write(genesis);
-      }
+   private static Genesis readGenesis(InputStream in) throws IOException, GenesisException {
+      return new GenesisReader(in).read();
+   }
 
-      PaintResult paint = GenesisPainter.paint(genesis);
-
-      Saver.saveEPS(paint, outputFile);
-      Saver.saveSVG(paint, outputFile);
-      Saver.savePNG(paint, outputFile);
+   private static void usage() {
+      System.err.println("Usage: materisaliser <operation>");
+      System.err.println();
+      System.err.println("      <operation>: generate-genesis | draw-png");
    }
 }
