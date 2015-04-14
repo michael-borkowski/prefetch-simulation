e40f2d0f6d94bbd2f2066237dc109c2447c2f324
Michael Borkowski <michael@borkowski.at>
2015-04-14 11:00:56 +0200

    fix(core/distributions): fix for negative modulus values

--- prefetch-simulation/core/src/main/java/at/borkowski/prefetchsimulation/configuration/distributions/UniformIntegerDistribution.java
+++ prefetch-simulation/core/src/main/java/at/borkowski/prefetchsimulation/configuration/distributions/UniformIntegerDistribution.java
@@ -4,18 +4,19 @@ import at.borkowski.prefetchsimulation.util.RandomSource;
 
 public class UniformIntegerDistribution implements Distribution<Integer> {
 
-   private final int min, max;
+   private final int min, max, span;
 
    public UniformIntegerDistribution(int min, int max) {
       this.min = min;
       this.max = max;
+      this.span = max - min;
    }
 
    @Override
    public Integer getValue(RandomSource randomSource) {
-      if (min == max)
+      if (span == 0)
          return min;
-      long r = randomSource.nextLong() % (max - min);
+      long r = ((randomSource.nextLong() % span) + span) % span;
       return (int) (min + r);
    }
 
--- prefetch-simulation/core/src/main/java/at/borkowski/prefetchsimulation/configuration/distributions/UniformLongDistribution.java
+++ prefetch-simulation/core/src/main/java/at/borkowski/prefetchsimulation/configuration/distributions/UniformLongDistribution.java
@@ -4,18 +4,19 @@ import at.borkowski.prefetchsimulation.util.RandomSource;
 
 public class UniformLongDistribution implements Distribution<Long> {
 
-   private final long min, max;
+   private final long min, max, span;
 
    public UniformLongDistribution(long min, long max) {
       this.min = min;
       this.max = max;
+      this.span = max - min;
    }
 
    @Override
    public Long getValue(RandomSource randomSource) {
-      if (min == max)
+      if (span == 0)
          return min;
-      long r = randomSource.nextLong() % (max - min);
+      long r = ((randomSource.nextLong() % span) + span) % span;
       return min + r;
    }
 
