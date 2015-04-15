fa72804a70c7e412ff2103a8dea468517e6b957e
Michael Borkowski <michael@borkowski.at>
2015-04-15 07:58:23 +0200

    fix(regression/S4): refined testing intveral: 0-36000 -> 0-1440

--- prefetch-simulation/regression/src/main/java/at/borkowski/prefetchsimulation/regression/variables/S4LookAheadTime.java
+++ prefetch-simulation/regression/src/main/java/at/borkowski/prefetchsimulation/regression/variables/S4LookAheadTime.java
@@ -10,7 +10,7 @@ public class S4LookAheadTime implements RegressionAnalysis {
    public void perform(RegressionContext context) {
       Configuration base = context.getBaseConfiguration();
 
-      for (long lookAheadTime = 0; lookAheadTime <= 36000; lookAheadTime += 720) {
+      for (long lookAheadTime = 0; lookAheadTime <= 400; lookAheadTime += 8) {
          Configuration configuration = new Configuration(base.getTotalTicks(), base.getByterate(), base.getSlotLength(), base.getNetworkUptime(), base.getRelativeJitter(), base.getAbsoluteJitter(), base.getRelativePredictionTimeError(), base.getRelativePredictionAmplitudeError(), base.getAbsolutePredictionTimeError(), base.getAbsolutePredictionAmplitudeError(), base.getRecurringRequestSeries(), base.getIntermittentRequests(), base.getAlgorithm(), base.getAlgorithmConfiguration(), lookAheadTime);
          context.executeRun(String.valueOf(lookAheadTime), configuration);
       }
