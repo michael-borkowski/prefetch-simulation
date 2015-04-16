d1409e3061479eda0da46062614aadee9cfaa6e6
Michael Borkowski <michael@borkowski.at>
2015-04-16 10:29:55 +0200

    fix(regression): S5 + S7: correct intervals

--- prefetch-simulation/regression/src/main/java/at/borkowski/prefetchsimulation/regression/variables/S5Alpha.java
+++ prefetch-simulation/regression/src/main/java/at/borkowski/prefetchsimulation/regression/variables/S5Alpha.java
@@ -13,7 +13,7 @@ public class S5Alpha implements RegressionAnalysis {
    public void perform(RegressionContext context) {
       Configuration base = context.getBaseConfiguration();
 
-      for (int i = 0; i <= 100; i += 2) {
+      for (int i = 24; i <= 100; i += 2) {
          Map<String, String> algorithmConfiguration = new HashMap<>(base.getAlgorithmConfiguration());
          algorithmConfiguration.put("alpha", String.valueOf(0.01 * i));
          Configuration configuration = new Configuration(base.getTotalTicks(), base.getByterate(), base.getSlotLength(), base.getNetworkUptime(), base.getRelativeJitter(), base.getAbsoluteJitter(), base.getRelativePredictionTimeError(), base.getRelativePredictionAmplitudeError(), base.getAbsolutePredictionTimeError(), base.getAbsolutePredictionAmplitudeError(), base.getRecurringRequestSeries(), base.getIntermittentRequests(), base.getAlgorithm(), algorithmConfiguration, base.getLookAheadTime());
